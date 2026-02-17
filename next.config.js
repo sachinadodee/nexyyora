26d3a35aa2b594a975b9b8b7d0ec994d3ad75bee
websitesachin25-code <websitesachin24@gmail.com>
2026-02-17 23:30:26 +0530

    config next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
+   output: 'export',
+   images: {
+    unoptimized: true
+   },
+   basePath:'\${nexyyora}',
+   assetPrefix:'\${nexyyora}'
 }
  
 
